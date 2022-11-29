ac62a39e5dd2ac52f040693d32c522a743d91a37
Arzedian <elianzas905@gmail.com>
2022-11-29 01:11:32 -0300

    IniciaProyecto

---
+++ src/main/java/com/proyectofinal_cfp18/App.java
@@ -0,0 +1,13 @@
+package com.proyectofinal_cfp18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
