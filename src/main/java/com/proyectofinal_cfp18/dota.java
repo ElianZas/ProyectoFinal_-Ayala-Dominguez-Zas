31c1e08b25c790e226de3642be55eecf06ac700a
LNPoly <paula.dominguez2205@gmail.com>
2022-12-04 13:30:18 -0300

    update dota.java

--- src/main/java/com/proyectofinal_cfp18/dota.java
+++ src/main/java/com/proyectofinal_cfp18/dota.java
@@ -1,17 +1,44 @@
 package com.proyectofinal_cfp18;
 
 public class dota {
+    
+    private int linea; // linea N°: 79
+    private int cantidad_unidades; //total de unidades: 12
+    private long Mantenimiento;
+    private long ganancias; // ganancias totales: 7000000
 
-    protected int linea; // linea N°: 79
-    protected int total_unidades; //total de unidades: 12
-    protected int total_ganancias; // ganancias totales: 7000000
-
-    public dota (int newLinea, int newTotal_unidades, int newTotal_ganancias){
+    public dota (int newLinea, int newCantidad_unidades,long newMantenimiento, long newGanancias){
         this.linea = newLinea;
-        this.total_unidades = newTotal_unidades;
-        this.total_ganancias = newTotal_ganancias;
+        this.Mantenimiento = newMantenimiento;
+        this.cantidad_unidades = newCantidad_unidades;
+        this.ganancias = newGanancias;
+    } 
+    
+    public int getLinea() {
+        return linea;
+    }
+    public int getCantidad_unidades() {
+        return cantidad_unidades;
+    }
+    public long getMantenimiento() {
+        return Mantenimiento;
+    }
+    public long getGanancias() {
+        return ganancias;
+    }
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+    public void setCantidad_unidades(int cantidad_unidades) {
+        this.cantidad_unidades = cantidad_unidades;
+    }
+    public void setMantenimiento(long mantenimiento) {
+        Mantenimiento = mantenimiento;
+    }
+    public void setGanancias(long ganancias) {
+        this.ganancias = ganancias;
+    }
 
-
-
-    }     
+    public static void add(dota mapDota) {
+    }  
 }
